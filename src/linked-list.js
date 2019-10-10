53670b4f42556c77539b735f2ffa658ce8e1bdcf
Perederieva <alin4ik.ua@gmail.com>
2019-10-10 16:50:07 +0300

    feat: add solution

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,27 +1,170 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
 
-    append(data) {}
+    constructor() {
+        this.length = 0;
+    }
 
-    head() {}
+    append(data) {
+        if (this.length === 0) {
+            this._head = new Node(data);
+            this._tail = this._head;
 
-    tail() {}
+        } else {
+            var node = new Node(data, this._tail);
+            this._tail.next = node;
+            this._tail = node;
+        }
 
-    at(index) {}
+        this.length++;
+        return this;
+    }
 
-    insertAt(index, data) {}
+    head() {
+        if (this.length === 0)
+            return null;
 
-    isEmpty() {}
+        return this._head.data;
+    }
 
-    clear() {}
+    tail() {
+        if (this.length === 0)
+            return null;
 
-    deleteAt(index) {}
+        return this._tail.data;
+    }
 
-    reverse() {}
+    at(index) {
+        if (this.length === 0) {
+            return null;
+        }
 
-    indexOf(data) {}
+        var currentIndex = 0;
+        var currentNode = this._head;
+
+        while (currentIndex <= index) {
+            if (index === currentIndex) {
+                return currentNode.data;
+            } else {
+                currentNode = currentNode.next;
+                currentIndex++;
+            }
+        }
+
+    }
+
+    insertAt(index, data) {
+
+        var currentIndex = 0;
+        var currentNode = this._head;
+
+        if(this.length === 0) {
+            this._head = new Node(data);
+            this.length++;
+            return;
+        }
+
+        while (currentIndex <= index) {
+            if (index === currentIndex) {
+                var node = new Node(data, currentNode.prev, currentNode);
+                currentNode.prev.next = node;
+                currentNode.prev = node;
+                this.length++;
+                break;
+            } else {
+                currentNode = currentNode.next;
+                currentIndex++;
+            }
+        }
+
+    }
+
+    isEmpty() {
+        return this.length === 0;
+    }
+
+    clear() {
+
+        var prevNode = this._tail;
+
+        while (this.length > 0) {
+            prevNode = this._tail.prev;
+            this._tail = null;
+            this._tail = prevNode;
+            this.length--;
+
+            if(this.length === 0)
+                this._head = null;
+        }
+
+        return this;
+    }
+
+    deleteAt(index) {
+
+        if(index === 0) {
+            var next = this._head.next;
+            this._head = null;
+            this._head = next;
+            this.length--;
+            return this;
+        }
+
+        var currentIndex = 0;
+        var currentNode = this._head;
+
+        while (currentIndex <= index) {
+            if (index === currentIndex) {
+                currentNode.prev.next = currentNode.next;
+                currentNode.next = currentNode.prev.next;
+                currentNode = null;
+                this.length--;
+                break;
+            } else {
+                currentNode = currentNode.next;
+                currentIndex++;
+            }
+        }
+        return this;
+    }
+
+    reverse() {
+        var currentNode = this._head;
+        var prevNode = null;
+        var nextNode = null;
+
+        while (currentNode !== null) {
+            nextNode = currentNode.next;
+            prevNode = currentNode.prev;
+
+            currentNode.next = prevNode;
+            currentNode.prev = nextNode;
+
+            prevNode = currentNode;
+            currentNode = nextNode;
+        }
+
+        this._tail = this._head;
+        this._head = prevNode;
+        return this;
+    }
+
+    indexOf(data) {
+        var currentIndex = 0;
+        var currentNode = this._head;
+
+        while (currentNode !== null) {
+            if (data === currentNode.data) {
+                return currentIndex;
+            } else {
+                currentIndex++;
+                currentNode = currentNode.next;
+            }
+        }
+
+        return -1;
+    }
 }
 
 module.exports = LinkedList;
